5a656f7f2f1d251bcaaea2c9062c834076ebd57b
Mike Wickwar <mike.wickwar@gmail.com>
2018-05-12 14:37:18 -0600

    can now create new chats

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -14,10 +14,14 @@ import ChatCtrl from  '../controllers/chat.controller';
 import ChatsCtrl from '../controllers/chats.controller';
 import ConfirmationCtrl from '../controllers/confirmation.controller';
 import LoginCtrl from '../controllers/login.controller';
+import NewChatCtrl from '../controllers/new-chat.controller';
 import ProfileCtrl from '../controllers/profile.controller';
 import SettingsCtrl from '../controllers/settings.controller';
 import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter';
+import ChatNameFilter from '../filters/chat-name.filter';
+import ChatPictureFilter from '../filters/chat-picture.filter';
+import NewChatService from '../services/new-chat.service';
 import Routes from '../routes';
 
 const App = 'Whatsapp';
@@ -34,10 +38,14 @@ new Loader(App)
   .load(ChatCtrl)
   .load(ConfirmationCtrl)
   .load(LoginCtrl)
+  .load(NewChatCtrl)
   .load(SettingsCtrl)
   .load(ProfileCtrl)
   .load(InputDirective)
   .load(CalendarFilter)
+  .load(ChatNameFilter)
+  .load(ChatPictureFilter)
+  .load(NewChatService)
   .load(Routes);
 
 // Startup
